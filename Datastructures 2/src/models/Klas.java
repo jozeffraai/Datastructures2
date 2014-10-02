7d3dad9dd703b981e8be44b31ab1a3f86abe79d9
BobPadje <bobpadje@live.nl>
2014-10-02 10:01:15 +0200

    Klas + student models

---
+++ Datastructures 2/src/models/Klas.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author bob
+ */
+public class Klas {
+     private int maxAantalLeerlingen;
+    private int aantalLeerlingen;
+    private String richting;
+}
---
+++ Datastructures 2/src/models/Student.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author bob
+ */
+public class Student {
+    private int studentNummer;
+    private String klas;
+    private double cijfer;
+}

2eef898efee1356bdaaaf49e7f26dfbc3c45495b
BobPadje <bobpadje@live.nl>
2014-10-02 11:12:32 +0200

    halve gare methode

--- Datastructures 2/src/datastructures/pkg2/Datastructures2.java
+++ Datastructures 2/src/datastructures/pkg2/Datastructures2.java
@@ -18,23 +18,49 @@ public class Datastructures2 {
     /**
      * @param args the command line arguments
      */
+    
+    private static String[] richting = {"IG", "IN", "IS", "IT"};
+    private static String[] laatsteGetal = {"1", "2", "3", "4"};
+    
     public static void main(String[] args) {
         int klasnummer = 50060001;
         double rangeMin = 1.0;
         double rangeMax = 10.0;
+        String klas;
+        int aantalLeerlingen = 8;
+        int richtingNummer = 0;
+        int getal = 0;
+        
         
         Random r = new Random();
         DecimalFormat df = new DecimalFormat("##.#");
+        
+        for (int i = 0; i < aantalLeerlingen; i++) {
+            
+        }
+        
+        
         
 //        Klas IS201 = new Klas("IS201", 20, "SE");
 //        Student jozef = new Student(50060001, "IS201", 5.5);
         
 //        System.out.println(df.format(cijfer));
         
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < aantalLeerlingen; i++) {
+            if (richtingNummer > 3){
+                richtingNummer = 0;
+            }
+            if (getal > 3){
+                getal = 0;
+            }
+            
+            klas = richting[richtingNummer] + "20" + getal;
+            richtingNummer++;
+            getal++;
+            
             double cijfer = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
             String result = df.format(cijfer);
-            Student a = new Student(klasnummer, "IS201", result);
+            Student a = new Student(klasnummer, klas, result);
             System.out.println(a.toString());
             klasnummer++;
         }

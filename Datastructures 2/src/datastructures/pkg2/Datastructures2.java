713ce01eac38b540d6e5ec7814abf926d66abad2
BobPadje <bobpadje@live.nl>
2014-10-08 13:03:44 +0200

    nog emer swag

--- Datastructures 2/src/datastructures/pkg2/Datastructures2.java
+++ Datastructures 2/src/datastructures/pkg2/Datastructures2.java
@@ -18,78 +18,72 @@ public class Datastructures2 {
     /**
      * @param args the command line arguments
      */
-    
     private static String[] richting = {"IG", "IN", "IS", "IT"};
     private static String[] laatsteGetal = {"1", "2", "3", "4"};
-    
+
     public static void main(String[] args) {
         int studentnummer = 50060001;
-        double rangeMin = 1.0;
-        double rangeMax = 10.0;
+
         String klas;
         int aantalLeerlingen = 800;
-        int b = aantalLeerlingen;
         int richtingNummer = 0;
-        int getal = 1;
+        int counter = 1;
         int aantalKlassen = 4;
         int aantalKlassenPerRichting;
-        
+
+        // bereken hoeveel klassen er moeten worden aangemaakt
         int aantalStudentRichting = aantalLeerlingen / 4;
         aantalKlassen = klassenAantal(aantalStudentRichting, aantalKlassen);
         aantalKlassenPerRichting = aantalKlassen / 4;
-        
-        System.out.println(aantalKlassen); 
-        System.out.println(aantalKlassenPerRichting);
-        
-        
-       
-        
+
+        // maak random generator aan
         Random r = new Random();
         DecimalFormat df = new DecimalFormat("##.0");
-        
-        
+
         System.out.println("Klas\t\tStudent Nummer  \tCijfer");
         for (int i = 0; i < aantalLeerlingen; i++) {
-            
-            if (richtingNummer > 3){
+
+            if (richtingNummer > 3) {
                 richtingNummer = 0;
             }
-            if (getal > 4){
-                getal = 1;
+            if (counter > 4) {
+                counter = 1;
             }
-            
-            klas = richting[richtingNummer] + "20" + getal;
+
+            klas = richting[richtingNummer] + "20" + counter;
             richtingNummer++;
-            
-            if(richtingNummer == 4)
-            getal++;
-            
-            double cijfer = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+
+            if (richtingNummer == 4) {
+                counter++;
+            }
+
+            // genereer een random cijfer
+            double cijfer = 1.0 + (10.0 - 1.0) * r.nextDouble();
             String result = df.format(cijfer);
-            // generate cijfer
-            
-            Klas k = new Klas(klas);
+
             // maak klas aan
-            Student a = new Student(studentnummer, result);
+            Klas k = new Klas(klas);
+
             // maak student aan 
+            Student a = new Student(studentnummer, result);
+
             k.addStudent(a);
-//            System.out.println(k.toString());
+            System.out.println(k.toString());
             studentnummer++;
         }
-        
-//        System.out.println(.toString);
+
+
     }
-    
-    
-    public static int klassenAantal(int aantalStudentRichting, int aantalKlassen){
-        
-        if(aantalStudentRichting < 33){
+
+    public static int klassenAantal(int aantalStudentRichting, int aantalKlassen) {
+
+        if (aantalStudentRichting < 33) {
             return aantalKlassen;
-        }else {
-            aantalStudentRichting = aantalStudentRichting - 32 ;
+        } else {
+            aantalStudentRichting = aantalStudentRichting - 32;
             aantalKlassen = aantalKlassen + 4;
             return klassenAantal(aantalStudentRichting, aantalKlassen);
         }
     }
-    
+
 }

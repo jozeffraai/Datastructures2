afd1687ad697b63687dfdb8fcba48cb917ee9304
fraaij001 <jozef.fraai@hva.nl>
2014-10-23 11:57:32 +0200

    Fuckin goeie werkende code G(inger)

--- Datastructures 2/src/datastructures/pkg2/Datastructures2.java
+++ Datastructures 2/src/datastructures/pkg2/Datastructures2.java
@@ -19,7 +19,7 @@ public class Datastructures2 {
     /**
      * @param args the command line arguments
      */
-    private static String[] richting = {"IG", "IN", "IS", "IT"};
+    private static String[] richting = {"G", "N", "S", "T"};
     private static String[] laatsteGetal = {"1", "2", "3", "4"};
 
     public static void main(String[] args) {
@@ -39,21 +39,18 @@ public class Datastructures2 {
 
         int counter = 0;
         int kn = 201;
-        for (int i = 0; i < aantalKlassen; i++) {
+        for (int i = 0; i < aantalKlassen+1; i++) {
 
             Klas k = new Klas();
 
             if (counter <= 3) {
-                k.setNaam(richting[counter] + kn);
+                k.setNaam("I"+richting[counter] + kn);
                 klassen.add(k);
-
                 counter++;
-                if (counter == 4) {
+            }else if (counter == 4) {
                     counter = 0;
                     kn++;
-                }
-            }
-
+                } 
         }
 
         Student[] studenten = new Student[aantalLeerlingen];
@@ -72,80 +69,22 @@ public class Datastructures2 {
             if (counter < klassen.size()) {
                 Klas k = klassen.get(counter);
                 s.setKlas(k);
+                counter++;
             }
+            if (counter == klassen.size()) {
+                    counter = 0;
+                } 
 
             studenten[i] = s;
+            System.out.println(counter);
         }
-
-        // maak random generator aan
-//        int kn = 201;
+        
         System.out.println("StudentNummer\tCijfer\tKlas");
         for (Student s : studenten) {
             System.out.println(s);
         }
 
         System.out.println(aantalKlassen);
-
-        for (int l = 0; l < aantalKlassenPerRichting; l++) {
-
-            Klas a1 = new Klas(richting[0] + kn);
-            Klas a2 = new Klas(richting[1] + kn);
-            Klas a3 = new Klas(richting[2] + kn);
-            Klas a4 = new Klas(richting[3] + kn);
-
-//            System.out.println(kn);
-            for (int i = 0; i < aantalLeerlingen; i++) {
-
-                // genereer een random cijfer
-                double cijfer1 = 1.0 + (10.0 - 1.0) * r.nextDouble();
-                String result1 = df.format(cijfer1);
-                double cijfer2 = 1.0 + (10.0 - 1.0) * r.nextDouble();
-                String result2 = df.format(cijfer2);
-                double cijfer3 = 1.0 + (10.0 - 1.0) * r.nextDouble();
-                String result3 = df.format(cijfer3);
-                double cijfer4 = 1.0 + (10.0 - 1.0) * r.nextDouble();
-                String result4 = df.format(cijfer4);
-
-                // maak student aan 
-                Student s1 = new Student(studentnummer, result1);
-                studentnummer++;
-                Student s2 = new Student(studentnummer, result2);
-                studentnummer++;
-                Student s3 = new Student(studentnummer, result3);
-                studentnummer++;
-                Student s4 = new Student(studentnummer, result4);
-                studentnummer++;
-
-//            System.out.println(s1);
-                studentnummer++;
-
-                a1.addStudent(s1);
-                a2.addStudent(s2);
-                a3.addStudent(s3);
-                a4.addStudent(s4);
-
-            }
-
-//            a1.showStudents();
-//            a2.showStudents();
-//            a3.showStudents();
-//            a4.showStudents();
-            kn++;
-        }
-
-//        System.out.println("Student Nummer  \tCijfer");
-//        for (int i = 0; i < aantalLeerlingen; i++) {
-//
-//            // genereer een random cijfer
-//            double cijfer = 1.0 + (10.0 - 1.0) * r.nextDouble();
-//            String result = df.format(cijfer);
-//
-//            // maak student aan 
-//            Student a = new Student(studentnummer, result);
-//
-//            System.out.println(a);
-//            studentnummer++;
-//        }
     }
 
     public static int klassenAantal(int aantalStudentRichting, int aantalKlassen) {

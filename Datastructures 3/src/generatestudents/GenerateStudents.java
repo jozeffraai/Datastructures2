dd97c3a719acb4a7a5db63cf1d3d9e99abfc9bb0
fraaij001 <jozef.fraai@hva.nl>
2014-10-27 14:19:00 +0100

    toAscii

--- Datastructures 3/src/generatestudents/GenerateStudents.java
+++ Datastructures 3/src/generatestudents/GenerateStudents.java
@@ -11,8 +11,32 @@ public class GenerateStudents {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         StudentList students = new StudentList(10000);
-        System.out.println(students);
+
+        
+        Student[] naam = students.getList();
+        
+        for (int i = 0; i < naam.length; i++) {
+            String test = naam[i].getLdap();
+            long modelo = toAscii(test) % 10007;
+            System.out.println(modelo);
+            
+        }
+        
     }
     
+    
+    public static long toAscii(String s){
+        StringBuilder sb = new StringBuilder();
+        String ascString = null;
+        long asciiInt;
+                for (int i = 0; i < s.length(); i++){
+                    sb.append((int)s.charAt(i));
+                    char c = s.charAt(i);
+                }
+                ascString = sb.toString();
+                asciiInt = Long.parseLong(ascString);
+                return asciiInt;
+    }
 }

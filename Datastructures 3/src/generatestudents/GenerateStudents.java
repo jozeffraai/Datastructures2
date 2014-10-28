6926a8d23d9b2bffec3051aa4a2011c58bf69c45
fraaij001 <jozef.fraai@hva.nl>
2014-10-29 00:29:32 +0100

    I'm Lost.....
    ik heb nu kunnen tellen hoeveel collisions je krijgt als je geen hash
    table gebruikt, deze resultaten heb ik ook in een grafiek gezet. we
    moetne nu die hashtables zelf maken en dan weer kijken hoeveel
    collisions we krijgen en daar weer tabellen van maken voor in ons
    verslag.

--- Datastructures 3/src/generatestudents/GenerateStudents.java
+++ Datastructures 3/src/generatestudents/GenerateStudents.java
@@ -1,4 +1,3 @@
-
 package generatestudents;
 
 import hashfunction.HashFunction;
@@ -13,32 +12,39 @@ public class GenerateStudents {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        StudentList students = new StudentList(20);
 
-        
+        StudentList students = new StudentList(10000);
+
         Student[] naam = students.getList();
-        long[] modeloNaam = new long[10000];
-        
+        int[] modeloNaam = new int[10000];
+
         for (int i = 0; i < naam.length; i++) {
-            String test = naam[i].getLdap();
-            long modelo = toAscii(test) % 10007;
-            System.out.println(modelo);
-            modeloNaam[i] = modelo;
+            String t = naam[i].getLdap();
+            int m = (int) toAscii(t);
+            modeloNaam[i] = m;
+        }
+
+        //Collisions tellen zonder hash table
+        int[] tellen = new int[97];
+        for (int i = 0; i < modeloNaam.length; i++) {
+            int waarde = modeloNaam[i];
+            tellen[waarde]++;
+        }
+        for (int i = 0; i < tellen.length; i++) {
+            System.out.println(tellen[i]);
         }
     }
-    
-    
-    public static long toAscii(String s){
+
+    public static long toAscii(String s) {
         StringBuilder sb = new StringBuilder();
         String ascString = null;
         long asciiInt;
-                for (int i = 0; i < s.length(); i++){
-                    sb.append((int)s.charAt(i));
-                    char c = s.charAt(i);
-                }
-                ascString = sb.toString();
-                asciiInt = Long.parseLong(ascString);
-                return asciiInt;
+        for (int i = 0; i < s.length(); i++) {
+            sb.append((int) s.charAt(i));
+            char c = s.charAt(i);
+        }
+        ascString = sb.toString();
+        asciiInt = Long.parseLong(ascString);
+        return asciiInt % 97;
     }
 }
